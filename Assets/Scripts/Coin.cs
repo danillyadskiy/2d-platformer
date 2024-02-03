81c358fdfc58dfcf46d64050dda4cebf4e21c2f9
Lyadskiy Daniil <danillyadskiy@gmail.com>
2024-02-03 13:54:58 +0300

    Coins added

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,13 +1,4 @@
 using System;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.TryGetComponent(out Bag bag))
-        {
-            bag.AddCoin();
-            Destroy(gameObject);
-        }
-    }
-}
+public class Coin : MonoBehaviour { }
